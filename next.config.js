f6e4a4cb18a5538509b9d3e30500c690abe72a46
salmamohmadsoror-cpu <salmamohmadsoror@gmail.com>
2026-09-09 23:30:01 +0300

    Add files via upload
    upload frontend files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.r2.dev" },
+      { protocol: "https", hostname: "**.cloudflare.com" }
+    ]
+  }
+};
+
 module.exports = nextConfig;
